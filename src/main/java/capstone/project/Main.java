e0daba56240adeec8733cc50301d3fc03a19f6eb
stevenhanwen <stevenhanwen.kang@outlook.com>
2025-05-21 23:29:03 -0400

    working on more methods

--- src/main/java/capstone/project/Main.java
+++ src/main/java/capstone/project/Main.java
@@ -6,8 +6,10 @@ public class Main
     public static void main( String[] args )
     {
        
-        PhysicalObject testObject = new PhysicalObject(10, 30, 20); 
-        System.out.println(testObject.maxHeight());
+        PhysicalObject testObject = new PhysicalObject(10, 90, 20); 
+        System.out.println(testObject.getMaxHeight());
+        System.out.println(testObject.getHorizontalDistanceTravelled());
+
         // testObject.show();
     }               
 
--- src/main/java/capstone/project/PhysicalObject.java
+++ src/main/java/capstone/project/PhysicalObject.java
@@ -13,27 +13,65 @@ public class PhysicalObject {
         this.initialVelocity = initialVelocity; 
     }
 
-    public double maxHeight(){
-        double maxHeight = (Math.pow(getVerticalVelocityComponent(), 2) / 9.81) - 
-        (Math.pow(getVerticalVelocityComponent(), 2) / 9.81 / 2) + heightOfLauch; 
+    // method to find the max height of the object after the launch. 
+    public double getMaxHeight(){
 
-        return maxHeight; 
+        if (angleOfLaunch <= 0) {
+            return heightOfLauch; 
+        }
+
+        // gravity of Earth = 9.807 m/s^2
+        double maxHeight = (Math.pow(getVerticalVelocityComponent(), 2) / 9.807) - 
+        (Math.pow(getVerticalVelocityComponent(), 2) / 9.807 / 2) + heightOfLauch; 
+
+        // rounding maxHeight to the thousandths place. 
+        return Math.round(maxHeight * 1000.0) / 1000.0;
     }
 
+    public double getTimeOfFlight() {
+
+        double time; 
+        time = (getVerticalVelocityComponent() + 
+        Math.pow(Math.pow(getVerticalVelocityComponent(), 2) - 2 * 9.807 * heightOfLauch, 0.5)) / 2;
+
+        return time; 
+    }
+
+    public double getHorizontalDistanceTravelled() {
+        double horizontalDistance = 0; 
+
+        if (angleOfLaunch == 90 || angleOfLaunch == -90) {
+            return 0.0; 
+        }
+
+        if (heightOfLauch > 0) {
+
+        }
+
+        return horizontalDistance; 
+    }
+
+
+    
+    // draw the object onto Java's JFrame
     public void show() {
         SimpleDraw circle = new SimpleDraw(null); 
         circle.showAnimation(); 
     }
 
+    // Helper function to find the horizontal velocity component of V initial. 
     public double getHorizontalVelocityComponent() {
         double radians = Math.toRadians(angleOfLaunch);
         double horizontalVelocity = initialVelocity * Math.cos(radians); 
+
         return horizontalVelocity; 
     }
 
+    // Helper function to find the vertical velocity component of V initial. 
     public double getVerticalVelocityComponent() {
         double radians = Math.toRadians(angleOfLaunch);
         double verticalVelocity = initialVelocity * Math.sin(radians); 
+
         return verticalVelocity; 
     }
 }
