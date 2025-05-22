4e8a3a0a3a03d6b3fa5ce33fc4534ed3663816dc
stevenhanwen <stevenhanwen.kang@outlook.com>
2025-05-22 16:43:27 -0400

    update unit test for time

--- src/test/java/capstone/project/PhysicalObjectTest.java
+++ src/test/java/capstone/project/PhysicalObjectTest.java
@@ -16,33 +16,44 @@ public class PhysicalObjectTest {
     }
 
     public PhysicalObject createPhysicalObject(double heightOfLaunch, double angleOfLaunch, double initialVelocity) {
-        PhysicalObject obj = new PhysicalObject(heightOfLaunch, angleOfLaunch, initialVelocity); 
-        return obj; 
+        PhysicalObject obj = new PhysicalObject(heightOfLaunch, angleOfLaunch, initialVelocity);
+        return obj;
     }
 
     @Test
     public void TestGetTimeOfFlight() {
 
-        PhysicalObject[] arrayOfPhysicalObjects = {
-            createPhysicalObject(0, 25, 10), 
-            createPhysicalObject(10, 10, 20), 
-            createPhysicalObject(5, 0, 15)
-        }; 
+        // PhysicalObject[] arrayOfPhysicalObjects = {
+        //         createPhysicalObject(0, 25, 10),
+        //         createPhysicalObject(10, 10, 20),
+        //         createPhysicalObject(5, 0, 15)
+        // };
 
-        double[] expectedTimes = {
-            0.86, 
-            1.83,
-            1.01
+        double[][] inputObjectParameters = {
+                // height, angle, initial velocity
+                { 0, 25, 10 },
+                { 10, 10, 20 },
+                { 5, 0, 15 },
+                // negative angle
+                { 5, -20, 15 }
         };
 
-        for (int i = 0; i < arrayOfPhysicalObjects.length; i++) {
-            assertEquals(expectedTimes[i], arrayOfPhysicalObjects[i].getTimeOfFlight());
+        double[] expectedTimes = {
+                0.86,
+                1.83,
+                1.01,
+                0.61
+        };
+
+        for (int i = 0; i < inputObjectParameters.length; i++) {
+            
+            PhysicalObject testObject = createPhysicalObject(inputObjectParameters[i][0], inputObjectParameters[i][1], inputObjectParameters[i][2]);
+            assertEquals(expectedTimes[i], testObject.getTimeOfFlight());
         }
 
-        
         PhysicalObject testObject = createPhysicalObject(5, 20, 10);
-        double calculatedTime = testObject.getTimeOfFlight(); 
-        double expectedTime = 1.42; 
+        double calculatedTime = testObject.getTimeOfFlight();
+        double expectedTime = 1.42;
         assertEquals(expectedTime, calculatedTime);
 
     }
